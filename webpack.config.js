66b63e528d5b46c3597fa06588cd7808731a0f90
phantummm <phantummm@gmail.com>
2017-04-16 12:30:28 -0700

    :sparkles: pull CSS into webpack build pipe

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: "./src/static/index.js",
+  entry: ["./src/static/index.css", "./src/static/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/'),
     publicPath: "/assets/",
     filename: "bundle.js"
   },
@@ -13,10 +14,23 @@ module.exports = {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader: "elm-webpack-loader?verbose=true&warn=true",
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          { fallback: 'style-loader', use: 'css-loader' }
+        )
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        loader: "file-loader"
       }
     ]
   },
   resolve: {
-    extensions: ["*", ".js", ".elm"]
+    extensions: [".js", ".elm"]
   },
+  plugins: [
+    new ExtractTextPlugin("bundle.css")
+  ]
 }
